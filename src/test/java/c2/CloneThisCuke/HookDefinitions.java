eb2dc6fdc01661d7ae5eca4547c6acf1fe5fdf58
jcarr-automation <45545420+jcarr-automation@users.noreply.github.com>
2020-02-13 13:23:44 -0600

    closes #10 and closes #11

---
+++ src/test/java/c2/CloneThisCuke/HookDefinitions.java
@@ -0,0 +1,46 @@
+package c2.CloneThisCuke;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
+@Configuration
+@PropertySource("classpath:framework.properties")
+public class HookDefinitions {
+	
+	@Autowired
+	static String browser;
+	
+	@Autowired
+	static String dev_run;
+
+	@Before
+	public void setup() {
+		Properties prop = new Properties();
+		try (InputStream input = getClass().getClassLoader().getResourceAsStream("framework.properties")) {
+			
+			// load a properties file
+            prop.load(input);
+
+            // get the property value and print it out
+            browser = prop.getProperty("browser");
+            dev_run = prop.getProperty("developer_run");
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+	}
+	
+	@After
+    public void teardown() {
+		
+        if (StepDefinitions.driver != null) {
+        	StepDefinitions.driver.quit();
+        }
+    }
+}
--- src/test/java/c2/CloneThisCuke/StepDefinitions.java
+++ src/test/java/c2/CloneThisCuke/StepDefinitions.java
@@ -1,15 +1,11 @@
 package c2.CloneThisCuke;
 
-import io.cucumber.java.After;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import static org.junit.Assert.*;
-
 import static org.junit.Assert.assertTrue;
-
 import java.util.List;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -23,33 +19,42 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class StepDefinitions {
 	
 	@Autowired
-	WebDriver driver;
+	static WebDriver driver;
+	
+	public WebDriver start_webdriver() {
+		
+		if(HookDefinitions.browser.equals("chrome")) {
+			ChromeOptions options = new ChromeOptions();
+		    options.addArguments("start-maximized");
+			System.setProperty(
+					"webdriver.chrome.driver", 
+					"resources\\drivers\\chromedriver.exe");
+		    driver = new ChromeDriver(options);
+		}
+		
+		if(HookDefinitions.browser.equals("edge")) {
+			System.setProperty(
+					"webdriver.edge.driver", 
+					"resources\\drivers\\msedgedriver.exe");
+		    driver = new EdgeDriver();
+		}
+		
+		if(HookDefinitions.browser.equals("firefox")) {
+			System.setProperty(
+					"webdriver.gecko.driver", 
+					"resources\\drivers\\geckodriver.exe");
+			FirefoxOptions options = new FirefoxOptions();
+			FirefoxDriver ff_driver = new FirefoxDriver(options);
+			driver = ff_driver;
+		}
+		
+		return driver;
+	}
 
 	@Given("navigate to cucumber home page")
 	public void navigate_to_cucumber_home_page() {
-		
-		// Chrome startup
-	    ChromeOptions options = new ChromeOptions();
-	    options.addArguments("start-maximized");
-		System.setProperty(
-				"webdriver.chrome.driver", 
-				"resources\\drivers\\chromedriver.exe");
-	    driver = new ChromeDriver(options);
-	    
-		// Edge startup
-//		System.setProperty(
-//				"webdriver.edge.driver", 
-//				"resources\\drivers\\msedgedriver.exe");
-//	    driver = new EdgeDriver();
-		
-	    // Firefox startup
-//		System.setProperty(
-//			"webdriver.gecko.driver", 
-//			"resources\\drivers\\geckodriver.exe");
-//		FirefoxOptions options = new FirefoxOptions();
-//        FirefoxDriver driver = new FirefoxDriver(options);
-	    
-	    driver.get("https://cucumber.io/");
+		start_webdriver();
+		driver.get("https://cucumber.io/");
 	}
 	
 	@When("link with {string} title is clicked")
@@ -65,11 +70,4 @@ public class StepDefinitions {
 				"Unable to locate the header text on the page!",
 				!textList.isEmpty());
 	}
-	
-	@After
-    public void teardown() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
 }
